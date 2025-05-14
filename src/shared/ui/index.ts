0a6b665043b4d0c6bb07f5d672711f00e64e903d
Vasiliy Vissarov <vissar2016@yandex.ru>
2025-05-15 00:27:53 +0300

    feat: profile-avatar component added

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,13 +1,12 @@
 export * from './button';
 export * from './profile-input';
 export * from './combined-input';
+export * from './profile-avatar';
 // export * from './block';
 // export * from './link';
-// export * from './avatar';
 // export * from './block';
 // export * from './dropdown';
 // export * from './modal';
 // export * from './chat-preview';
 // export * from './message-item';
 // export * from './profile-input';
-// export * from './search-input';
---
+++ src/shared/ui/profile-avatar/index.ts
@@ -0,0 +1,2 @@
+import './style.css';
+export {default as UiProfileAvatar} from './profile-avatar.hbs?raw'

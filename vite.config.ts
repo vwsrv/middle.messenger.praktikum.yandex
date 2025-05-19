61a866ddde74da2abad1f6bea313ab0634d88b9c
Vasiliy Vissarov <vissar2016@gmail.com>
2025-05-19 13:15:21 +0300

    fix: autotests fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    server: {
-        port: 3000
-    },
-    preview: {
-        port: 3000
-    }
-})
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+});
